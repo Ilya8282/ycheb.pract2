80e9820e74a9f63f0334a324d0926736d8dd22c9
Ilya8282 <122673415+Ilya8282@users.noreply.github.com>
2023-12-04 12:17:27 +0300

    pract2
    pract2

---
+++ cr_pr1/app/src/main/java/com/example/myapplication/MainActivity4.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+
+}
